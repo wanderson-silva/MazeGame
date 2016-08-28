4cda8b5549760900eed63ec08feb92c8fe2159a4
Wanderson Silva <wandersonwod@gmail.com>
2016-08-28 04:05:32 -0400

    commit inicial

---
+++ LabirintoGamer/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabirintoGamer
+{
+    class Program
+    {   // Título do jogo
+        static string tituloDoJogo = "Jogo de Labirinto";
+
+        static string[,] labirinto = new string[8, 16]
+        {
+            {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#" },
+            {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#" },
+            {"#","#","#","#","#","#","#","#","#"," "," "," "," "," ","#","#" },
+            {"#","#","#","#"," "," "," ","#","#"," ","#","#","#"," ","#","#" },
+            {"#"," "," "," "," ","#"," ","#","#"," ","#"," "," "," ","#","#" },
+            {"#"," ","#","#","#","#"," "," "," "," ","#"," ","#","#"," ","F" },
+            {"#"," ","#","#","#","#","#","#","#","#","#"," "," "," "," ","#" },
+            {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#" },
+
+        };
+
+        static string jogador = "^";
+        static int posLinha = 6;
+        static int posColuna = 1;
+
+        static void Main(string[] args)
+        {
+            // Título do jogo
+            Console.Title = tituloDoJogo;
+
+            bool fimDeJogo = false;
+
+            // exibindo o jogador no labirinto
+            labirinto[6, 1] = jogador;
+
+            while (!fimDeJogo)
+            {
+                Console.Clear();
+                // Linha do labirinto
+                for (int linha = 0; linha < 8; linha++)
+                {
+                    // Coluna
+                    for (int coluna = 0; coluna < 16; coluna++)
+                    {
+                        Console.Write(labirinto[linha, coluna]);
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Para onde você deseja ir?");
+                Console.WriteLine("> W = Cima");
+                Console.WriteLine("> A = Esquerda");
+                Console.WriteLine("> S = Baixo");
+                Console.WriteLine("> D = Direita");
+                Console.Write("Meu comando é :");
+
+                string comando = Console.ReadLine().ToUpper();
+
+                // Instruções de movimento
+                if (comando == "W")
+                {
+                    if (labirinto[posLinha - 1, posColuna] != "#")
+                    {
+                        labirinto[posLinha, posColuna] = " ";
+                        posLinha--;
+                    }
+                    else
+                    {
+                        fimDeJogo = true;
+                    }
+                }
+                else if (comando == "A")
+                {
+                    if (labirinto[posLinha, posColuna - 1] != "#")
+                    {
+                        labirinto[posLinha, posColuna] = " ";
+                        posColuna--;
+                    }
+                    else
+                    {
+                        fimDeJogo = true;
+                    }
+                }
+                else if (comando == "S")
+                {
+                    if (labirinto[posLinha + 1, posColuna] != "#")
+                    {
+                        labirinto[posLinha, posColuna] = " ";
+                        posLinha++;
+                    }
+                    else
+                    {
+                        fimDeJogo = true;
+                    }
+                }
+                else if (comando == "D")
+                {
+                    if (labirinto[posLinha, posColuna + 1] != "#")
+                    {
+                        labirinto[posLinha, posColuna] = " ";
+                        posColuna++;
+                    }
+                    else
+                    {
+                        fimDeJogo = true;
+                    }
+                }
+                
+
+                labirinto[posLinha, posColuna] = jogador;
+
+                if(labirinto[5, 15] == jogador)
+                {
+                    fimDeJogo = true;
+                }
+
+            }
+
+            if (labirinto[5, 15] == jogador)
+            {
+                Console.Clear();
+                Console.WriteLine("PARABÉNS!!! VOCÊ CHEGOU AO FIM DO LABIRINTO!");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("GAME OVER!");
+            }
+
+            Console.WriteLine("Tecle ENTER para finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+#Game Maze
+Game Maze for console application developed during my studies of C#.
